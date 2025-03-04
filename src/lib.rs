d13013b227bf27a9a0973cfd30a75f1aa84fa9c2
Sam Lloyd <samjlloyd1@gmail.com>
2025-03-04 17:57:17 +1100

    graceful shutdown handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+
 use std::{
     sync::{mpsc, Arc, Mutex},
     thread,
@@ -5,7 +6,7 @@ use std::{
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -32,7 +33,7 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        ThreadPool { workers, sender: Some(sender) }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -41,7 +42,19 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop (&mut self) {
+        drop(self.sender.take());
+        
+        for worker in &mut self.workers.drain(..) {
+            println!("shutting worker down {}", worker.id);
+        
+            worker.thread.join().unwrap();
+        }
     }
 }
 
@@ -52,12 +65,20 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-
-            println!("Worker {id} got a job; executing.");
-
-            job();
+            let message = receiver.lock().unwrap().recv();
+            
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                },
+                Err(_) => {
+                    println!("worker {id} disconnected, shutting down.");
+                    break;
+                }
+            }
         });
 
         Worker { id, thread }
