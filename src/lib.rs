4565fb3778d69b7886f0019c9d2586bc5f38dc36
Sam Lloyd <samjlloyd1@gmail.com>
2025-03-04 17:58:16 +1100

    lint and remove comments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 use std::{
     sync::{mpsc, Arc, Mutex},
     thread,
@@ -23,7 +22,7 @@ impl ThreadPool {
         assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
-        
+
         let receiver = Arc::new(Mutex::new(receiver));
 
         let mut workers = Vec::with_capacity(size);
@@ -33,7 +32,10 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender: Some(sender) }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -47,12 +49,12 @@ impl ThreadPool {
 }
 
 impl Drop for ThreadPool {
-    fn drop (&mut self) {
+    fn drop(&mut self) {
         drop(self.sender.take());
-        
+
         for worker in &mut self.workers.drain(..) {
             println!("shutting worker down {}", worker.id);
-        
+
             worker.thread.join().unwrap();
         }
     }
@@ -65,15 +67,14 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        
         let thread = thread::spawn(move || loop {
             let message = receiver.lock().unwrap().recv();
-            
+
             match message {
                 Ok(job) => {
                     println!("Worker {id} got a job; executing.");
                     job();
-                },
+                }
                 Err(_) => {
                     println!("worker {id} disconnected, shutting down.");
                     break;
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,25 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    time::Duration,
     thread,
+    time::Duration,
 };
 use web_server_rust_book::ThreadPool;
 
 fn main() {
-    
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        
-        pool.execute(|| {   
+
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
@@ -33,18 +31,18 @@ fn handle_connection(mut stream: TcpStream) {
             let status_line = "HTTP/1.1 200 OK";
             let contents = fs::read_to_string("hello.html").unwrap();
             let length = contents.len();
-            
+
             let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-            
+
             stream.write_all(response.as_bytes()).unwrap();
-        },
+        }
         "GET /foo HTTP/1.1" => {
             let status_line = "HTTP/1.1 200 OK";
             let contents = fs::read_to_string("foo.html").unwrap();
             let length = contents.len();
-            
+
             let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-            
+
             stream.write_all(response.as_bytes()).unwrap();
         }
         "GET /sleep HTTP/1.1" => {
@@ -52,32 +50,11 @@ fn handle_connection(mut stream: TcpStream) {
             let status_line = "HTTP/1.1 200 OK";
             let contents = fs::read_to_string("foo.html").unwrap();
             let length = contents.len();
-            
+
             let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-            
+
             stream.write_all(response.as_bytes()).unwrap();
         }
         _ => {}
-
     }
-
-    // if request_line == "GET / HTTP/1.1" {
-    //     let status_line = "HTTP/1.1 200 OK";
-    //     let contents = fs::read_to_string("hello.html").unwrap();
-    //     let length = contents.len();
-        
-    //     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        
-    //     stream.write_all(response.as_bytes()).unwrap();
-    // }
-    // else if request_line == "GET /foo HTTP/1.1" {
-    //     let status_line = "HTTP/1.1 200 OK";
-    //     let contents = fs::read_to_string("foo.html").unwrap();
-    //     let length = contents.len();
-        
-    //     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        
-    //     stream.write_all(response.as_bytes()).unwrap();
-    // }
-
 }
